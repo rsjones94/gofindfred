0878265f51365b4f8792e57f5d28ecac1ef76564
Sky Jones <rsajones94@gmail.com>
2019-07-17 21:22:19 -0500

    no message

--- app/events/admin.py
+++ app/events/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'notes']
